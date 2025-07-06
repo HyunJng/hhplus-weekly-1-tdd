8e6afaafb20d52fbf55c150ea5425ce32e129eac
HyunJng <khjung1654@gmail.com>
2025-07-06 19:38:19 +0900

    [feat] 유저의 포인트 충전/이용 내역 조회 기능 추가

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,17 +1,29 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.point.domain.PointHistory;
+import io.hhplus.tdd.point.infrastruction.PointHistoryTable;
 import io.hhplus.tdd.point.infrastruction.UserPointTable;
 import io.hhplus.tdd.point.domain.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint findUserPoint(Long id) {
         return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> findUserPointLog(Long id) {
+        return pointHistoryTable.selectAllByUserId(id).stream()
+                .sorted(Comparator.comparingLong(PointHistory::id).reversed())
+                .toList();
     }
 }
--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,9 +1,15 @@
 package io.hhplus.tdd.point.service;
 
-import io.hhplus.tdd.point.infrastruction.UserPointTable;
+import io.hhplus.tdd.point.domain.PointHistory;
+import io.hhplus.tdd.point.domain.TransactionType;
 import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.infrastruction.PointHistoryTable;
+import io.hhplus.tdd.point.infrastruction.UserPointTable;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,14 +17,21 @@ class PointServiceTest {
 
     private PointService pointService;
     private UserPointTable pointTable;
+    private PointHistoryTable pointHistoryTables;
 
     @BeforeEach
     void init() {
         pointTable = new UserPointTable();
-        pointService = new PointService(pointTable);
+        pointHistoryTables = new PointHistoryTable();
+        pointService = new PointService(pointTable, pointHistoryTables);
 
         pointTable.insertOrUpdate(1L, 1000);
         pointTable.insertOrUpdate(2L, 80000);
+
+        pointHistoryTables.insert(1L, 1000, TransactionType.CHARGE, 1234567891012L);
+        pointHistoryTables.insert(2L, 100000, TransactionType.CHARGE, 1334587891012L);
+        pointHistoryTables.insert(2L, 30000, TransactionType.USE, 1434587891012L);
+        pointHistoryTables.insert(2L, 10000, TransactionType.CHARGE, 1534587891012L);
     }
 
     @Test
@@ -41,4 +54,36 @@ class PointServiceTest {
         assertThat(userPoint.point()).isEqualTo(0);
     }
 
+    @Test
+    void 유저의_포인트_이용_내역을_조회할_수_있다() throws Exception {
+        //given & when
+        List<PointHistory> userPointLog = pointService.findUserPointLog(2L);
+
+        //then
+        assertThat(userPointLog.size()).isEqualTo(3);
+        Assertions.assertAll(
+                () -> assertThat(userPointLog.get(0).userId()).isEqualTo(2L),
+                () -> assertThat(userPointLog.get(0).amount()).isEqualTo(10000),
+                () -> assertThat(userPointLog.get(0).type()).isEqualTo(TransactionType.CHARGE)
+        );
+        Assertions.assertAll(
+                () ->assertThat(userPointLog.get(1).userId()).isEqualTo(2L),
+                () -> assertThat(userPointLog.get(1).amount()).isEqualTo(30000),
+                () -> assertThat(userPointLog.get(1).type()).isEqualTo(TransactionType.USE)
+        );
+        Assertions.assertAll(
+                () ->assertThat(userPointLog.get(2).userId()).isEqualTo(2L),
+                () -> assertThat(userPointLog.get(2).amount()).isEqualTo(100000),
+                () -> assertThat(userPointLog.get(2).type()).isEqualTo(TransactionType.CHARGE)
+        );
+    }
+
+    @Test
+    void 존재하지_않는_유저의_포인트_이용_내역을_조회_요청_시_빈_리스트를_반환한다() throws Exception {
+        //given & when
+        List<PointHistory> userPointLog = pointService.findUserPointLog(1000L);
+
+        //then
+        assertThat(userPointLog.size()).isEqualTo(0);
+    }
 }
